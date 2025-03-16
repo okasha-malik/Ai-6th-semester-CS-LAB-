43018fcc6392cdcb0ca5af386c8d3547d4392fb2
okasha-malik <okashamalik121@gmail.com>
2025-03-16 17:48:03 +0500

    Add files via upload

---
+++ lab2 ai bcs22038 6 th sem.py
@@ -0,0 +1,117 @@
+# count= 0
+# while(count<5):
+#     count=count+1
+#     print("i study in pugc")
+
+    # single statement
+# count=0
+# while(count==0
+#     ):
+        
+        # print("i study in pugc")
+
+# example 1 for loop
+# ----------------------
+# print("hello")
+# l= ["geek","for","geek"]
+# for i in l :
+#     print(i)
+# ----------------------
+# example 2 for loop
+
+# print("\n tuple iteration")
+# l= ["geek","for","geek"]
+# for i in l :
+#     print(i)
+# ----------------------
+    # example 3 for loop
+
+# print("\n string iteration")
+# y= "geeks"
+# for i in y :
+#     print(i)
+
+# ----------------------
+# loop control
+
+# list= ["geek","for","geek"]
+# for i in range(len (list)):
+#  print (list[i])
+
+
+# for i in 'geek for geek':
+#     if i=='e 'or i=='s':
+#         continue
+# print('current letter:'), i
+# var = 10
+
+
+# function 
+# ----------------------
+
+# def my_function():
+#     print("hikkkkkkkk")
+
+# my_function()
+
+# ----------------------
+# passing of a function
+
+
+# r=3
+# def function( x):
+#     y=x+3
+#     return y
+# print (function(r))
+
+
+# ----------------------
+
+
+# def fun(country= "pak" ):
+#     print( "i  from"+ country)
+
+# fun("sweden")
+
+# ----------------------
+
+# def fun(food):
+#  for x in food:
+#     print (x)
+# fruits=["apple" ,"grapes","mango"]   
+
+# fun(fruits)
+# ----------------------
+
+
+# def fun(x):
+#  return (6*x)
+
+# print(fun(4))
+
+#  ----------------------
+# def child(one,two,tgree):
+#     print("youngest ", two)
+
+# child(one="a",two="b",tgree="c")
+
+#  ----------------------
+# class person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+# p=person("okasha",30)
+# print(p.name,p.age)
+#output:
+#okasha 30
+
+
+#  ----------------------
+# class person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#     def my_fun(self):
+#         print("Hello my name is", self.name)
+# p=person("okasha",30)
+# p.my_fun()
